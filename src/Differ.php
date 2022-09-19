a1cd7f2bf578c76247238fdcd0086d1194055c61
Ivan Popov <popovbm@gmail.com>
2022-09-19 21:23:56 +0300

    step 3 3

--- src/Differ.php
+++ src/Differ.php
@@ -4,28 +4,60 @@ namespace gendiff\Differ;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
-    $file1 = json_decode($pathToFile1, true);
-    $file2 = json_decode($pathToFile2, true);
+    $file1 = file_get_contents($pathToFile1);
+    $file2 = file_get_contents($pathToFile2);
 
-    $mergeFiles = array_merge($file1, $file2);
+    $decodeFile1 = json_decode($pathToFile1, true);
+    $decodeFile2 = json_decode($pathToFile2, true);
 
-    $keys = array_keys($mergeFiles);
+    $mergeFiles = array_merge($decodeFile1, $decodeFile2);
 
-    sort($keys);
+    $filesKeys = array_keys($mergeFiles);
 
-    $result = array_reduce($keys, function ($acc, $key) use ($file1, $file2) {
-        if (!array_key_exists($key, $file2)) {
-            $acc[] = "- {$key}: {$file1[$key]}";
-        } elseif (!array_key_exists($key, $file1)) {
-            $acc[] = "+ {$key}: {$file2[$key]}";
-        } elseif ($file1[$key] === $file2[$key]) {
-            $acc[] = "  {$key}: {$file1[$key]}";
-        } else{
-            $acc[] = "- {$key}: {$file1[$key]}";
-            $acc[] = "+ {$key}: {$file2[$key]}";
-        }
-        return $acc;
-    }, []);
+    sort($filesKeys);
 
-    return $result;
+    $toString = function ($val) {
+        return trim(var_export($val, true), "'");
+    };
+
+    $diff = function ($keys) use ($decodeFile1, $decodeFile2, $toString) {
+        $result = array_reduce($keys, function ($acc, $key) use ($decodeFile1, $decodeFile2, $toString) {
+            if (!array_key_exists($key, $decodeFile1)) {
+                $acc[] = "+ {$key}: {$toString($decodeFile2[$key])}";
+            } elseif (!array_key_exists($key, $decodeFile2)) {
+                $acc[] = "- {$key}: {$toString($decodeFile1[$key])}";
+            } elseif ($decodeFile1[$key] === $decodeFile2[$key]) {
+                $acc[] = "  {$key}: {$toString($decodeFile1[$key])}";
+            } else {
+                $acc[] = "- {$key}: {$toString($decodeFile1[$key])}";
+                $acc[] = "+ {$key}: {$toString($decodeFile2[$key])}";
+            }
+            return $acc;
+        }, []);
+
+        return implode("\n", $result);
+    };
+
+    return $diff($filesKeys);
 }
+
+$doc = <<<DOC
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
+DOC;
+
+require('../vendor/docopt/docopt/src/docopt.php');
+$args = Docopt::handle($doc, array('version'=>'Naval Fate 2.0'));
+foreach ($args as $k=>$v)
+echo $k.': '.json_encode($v).PHP_EOL;
